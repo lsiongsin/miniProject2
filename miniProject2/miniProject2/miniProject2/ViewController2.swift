67cfb975bded3fccc3bf4ae4d2966a8ad27a9864
lsiongsin <110996784+lsiongsin@users.noreply.github.com>
2022-08-11 14:34:04 -0400

    upload1

---
+++ miniProject2/miniProject2/miniProject2/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  miniProject2
+//
+//  Created by Scholar on 8/11/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBOutlet weak var x: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
